4e55550a92d15b0b8232a0fe8177eb6b286f24eb
Bruno Silva <bruno3dcontato@gmail.com>
2019-09-06 10:38:19 -0300

    Update githubAuth.js

--- routes/githubAuth.js
+++ routes/githubAuth.js
@@ -156,22 +156,24 @@ module.exports.verifySession = async (app, req, res) => {
 					console.error(`Usuário não encontrado: ${commentDB.userID}`);
 					console.log("========================", "Tentando criar novo usuário", comments, "========================");
 
-					const { email, id, login, name: displayName, html_url, avatar_url } = user_response.data;
+					if (user_response.data) {
+						const { email, id, login, name: displayName, html_url, avatar_url } = user_response.data;
 
-					try {
-						const dev = await UserModel.create({
-							email,
-							id,
-							login,
-							displayName,
-							profileURL: html_url,
-							avatarURL: avatar_url,
-						});
+						try {
+							const dev = await UserModel.create({
+								email,
+								id,
+								login,
+								displayName,
+								profileURL: html_url,
+								avatarURL: avatar_url,
+							});
 
-						res.redirect("/home");
-					}
-					catch (error) {
-						console.error(`Falha ao criar novo usuário: ID:${id}, login:${login}, details:`, error);
+							return res.redirect("/home");
+						}
+						catch (error) {
+							console.error(`Falha ao criar novo usuário: ID:${id}, login:${login}, details:`, error);
+						}
 					}
 				}
 			});
